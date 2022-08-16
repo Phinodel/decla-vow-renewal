99b98aaf183797331f0072e7057e0a8e6a605902
Delphine Claerhout <d.claerhout@cultura.fr>
2022-08-16 16:12:41 +0200

    added meta tags

--- components/pageLayout/index.tsx
+++ components/pageLayout/index.tsx
@@ -20,9 +20,17 @@ const Layout: FC<Props> = ({ children }) => {
         <meta name="description" content={t('description')} />
         <link rel="icon" href="/favicon.ico" />
 
+        <meta property="og:url" content="https://decla-vow-renewal.vercel.app/" />
         <meta property="og:title" content={t('title')} />
         <meta property="og:description" content={t('description')} />
-        <meta property="og:image" content="/favicon.ico" />
+
+        <meta property="og:image" content="https://decla-vow-renewal.vercel.app/favicon.ico" />
+        <meta property="twitter:image" content="https://decla-vow-renewal.vercel.app/favicon.ico" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="180" />
+        <meta property="og:image:height" content="180" />
+
+        <meta property="og:type" content="website" />
 
         <meta name="author" content="Delphine Claerhout" />
       </Head>
